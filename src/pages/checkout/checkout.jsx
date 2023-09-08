510adc2a8ad5148825704691906c115262973758
Sourabh Bagrecha <bagrechasourabh622@gmail.com>
2023-09-08 10:44:00 +0530

    project uploaded

---
+++ src/pages/checkout/checkout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./checkout.css";
+
+
+
+export const Checkout = () => {
+  return (
+    <div className="checkout">
+      <h1> WoW..!! One Step More, </h1>
+      <h2 className="checkout"> Payment Page </h2>
+      <button className="paybtn" onClick={() => { }} >
+            {" "}
+            To Pay{" "} </button>
+    </div>
+    );
+}
